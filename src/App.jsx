b25040b273e60a2e6f2dbd20734239a246c1c06b
karan <karankumar46226@gmail.com>
2024-01-09 21:17:20 +0530

    des and ratings

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import Ziggy from './components/Ziggy'
 import Ziggy2 from './components/Ziggy2'
 import Ziggy3 from './components/Ziggy3'
 import Ziggy4 from './components/Ziggy4'
+import Ziggy5 from './components/Ziggy5'
 
 function App() {
 
 
   return (
-    <Ziggy4/>
+    <Ziggy5/>
+    // <Ziggy4/>
     // <Ziggy3/>
     // <Ziggy2/>
     // <Ziggy/>
